97efa8e086001431f9f97457bc8ce9b4a422103b
IDEASK <9982733@qq.com>
2019-09-18 23:56:15 +0800

    本次上传更新内容：
    1.expandRoi增加图片前后对比时的文字提示；
    2.生成train/test.txt，每行对应一张扩展人脸，.jpg为原图位置，后续4个数字为expand后的人脸边框坐标，后续42个数字为相对于人脸边框的人脸关键点坐标；
    3.为确保生成数据的准确性，增加的验证函数testTrainTestTxt；

--- dataProcess.py
+++ dataProcess.py
@@ -1,6 +1,8 @@
 import cv2
 import os
 import numpy as np
+import random as rd
+
 
 folderList = ['I', 'II']
 
@@ -55,7 +57,7 @@ def drawRectLandMarks(truth):
 # truth = loadRectLandMarks(loadMetaDataList())
 # drawRectLandMarks(truth)
 
-def expand_roi(x1, y1, x2, y2, imgWidth, imgHeight, ratio):
+def expandRoi(x1, y1, x2, y2, imgWidth, imgHeight, ratio):
     width = x2 - x1 + 1
     height = y2 - y1 + 1
     paddingWidth = int(width * ratio)
@@ -81,9 +83,11 @@ def drawRectLandMarksExpandOri(truth, ratio):
         for rect in truth[line]:
             x1, y1 = rect[0][0], rect[0][1]
             x2, y2 = rect[0][2], rect[0][3]
-            roiX1, roiY1, roiX2, roiY2, roiWidth, roiHeight = expand_roi(x1, y1, x2, y2, width, height, ratio) # ratio usually 0.25
+            roiX1, roiY1, roiX2, roiY2, roiWidth, roiHeight = expandRoi(x1, y1, x2, y2, width, height, ratio) # ratio usually 0.25
             cv2.rectangle(imgData, (x1, y1), (x2, y2), (255, 0, 0), 3)
+            cv2.putText(imgData, 'not expand:', (0, 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 199, 0), 1, 1)
             cv2.rectangle(imgDataExpandRoi, (roiX1, roiY1), (roiX2, roiY2), (0, 255, 0), 3)
+            cv2.putText(imgDataExpandRoi, 'expand:', (0, 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 199, 0), 1, 1)
             for landMark in rect[1]:
                 cv2.circle(imgData, (landMark[0], landMark[1]), 1, (0, 0, 255), 1)
                 cv2.circle(imgDataExpandRoi, (landMark[0], landMark[1]), 1, (0, 0, 255), 1)
@@ -95,3 +99,66 @@ def drawRectLandMarksExpandOri(truth, ratio):
 # truth = loadRectLandMarks(loadMetaDataList())
 # drawRectLandMarksExpandOri(truth, 0.25)
 
+def createExpandRoiTxt(metaDataList, ratio, scale): # scale为训练数据所占比例90%或者70%均能接受
+    total = len(metaDataList)
+    trainNums = int(total * scale) if int(total * scale) < total else total
+    testNums = total - trainNums
+    print((trainNums,testNums))
+    write_lines = []
+    for line in metaDataList:
+        line = line.strip().split()
+        picDir = line[0]
+        imgData = cv2.imread(picDir)
+        picWidth = imgData.shape[1]
+        picHeight = imgData.shape[0]
+        rect = list(map(int, list(map(float, line[1:5]))))
+        x1, y1, x2, y2 = rect[0], rect[1], rect[2], rect[3]
+        roiX1, roiY1, roiX2, roiY2, roiWidth, roiHeight = expandRoi(x1, y1, x2, y2, picWidth, picHeight, ratio)
+        keyPointList = []
+        for item in line[5:]:
+            keyPointList.append(float(item))
+        keyPointList = np.array(keyPointList)
+        keyPointList = keyPointList.reshape(21, 2)
+        keyPointList -= np.array([roiX1, roiY1])
+        keyPointList = keyPointList.flatten()
+        # print(keyPointList)
+        write_line = picDir + ' ' + str(roiX1) + ' ' + str(roiY1) + ' ' + str(roiX2) + ' ' + str(roiY2)
+        for word in keyPointList:
+            write_line += ' ' + str(word)
+        write_line += '\n'
+        write_lines.append(write_line)
+    rd.shuffle(write_lines)
+    with open('train.txt', 'w') as wf:
+        for wl in write_lines[:trainNums]:
+            wf.write(wl)
+    with open('test.txt', 'w') as wf:
+        for wl in write_lines[trainNums:]:
+            wf.write(wl)
+
+#createExpandRoiTxt(loadMetaDataList(), 0.25, 0.9)
+
+# def testTrainTestTxt(filename):
+#     truth = {}
+#     with open(filename) as f:
+#         lines = f.readlines()
+#     for line in lines:
+#         line = line.strip().split()
+#         imgDir = line[0]
+#         if imgDir not in truth:
+#             truth[imgDir] = []
+#         rect = list(map(int, list(map(float, line[1:5]))))
+#         x = list(map(int, list(map(float, line[5::2]))))
+#         y = list(map(int, list(map(float, line[6::2]))))
+#         landMarks = list(zip(x, y))
+#         truth[imgDir].append((rect, landMarks))
+#     for line in truth:
+#         imgData = cv2.imread(line)
+#         for rect in truth[line]:
+#             cv2.rectangle(imgData, (rect[0][0], rect[0][1]), (rect[0][2], rect[0][3]), (0, 255, 0), 3)
+#             for landMark in rect[1]:
+#                 cv2.circle(imgData, (landMark[0] + rect[0][0], landMark[1] + rect[0][1]), 1, (0, 0, 255), 1)
+#         cv2.imshow(line, imgData)
+#         cv2.waitKey(0)
+#         cv2.destroyAllWindows()
+#
+# testTrainTestTxt('train.txt')
